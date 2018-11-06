5bab8fd28e72535e8903ab266506ece77dbce6d1
Prabesh Bhaskaran <prabeshb@gmail.com>
2018-11-06 18:02:45 -0500

    Assignment-1 completed

---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+const DISH = {
+  id: '0',
+  name: 'Uthappizza',
+  image: '/assets/images/uthappizza.png',
+  category: 'mains',
+  featured: true,
+  label: 'Hot',
+  price: '4.99',
+  // tslint:disable-next-line:max-line-length
+  description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+  comments: [
+       {
+           rating: 5,
+           comment: 'Imagine all the eatables, living in conFusion!',
+           author: 'John Lemon',
+           date: '2012-10-16T17:57:28.556094Z'
+       },
+       {
+           rating: 4,
+           comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+           author: 'Paul McVites',
+           date: '2014-09-05T17:57:28.556094Z'
+       },
+       {
+           rating: 3,
+           comment: 'Eat it, just eat it!',
+           author: 'Michael Jaikishan',
+           date: '2015-02-13T17:57:28.556094Z'
+       },
+       {
+           rating: 4,
+           comment: 'Ultimate, Reaching for the stars!',
+           author: 'Ringo Starry',
+           date: '2013-12-02T17:57:28.556094Z'
+       },
+       {
+           rating: 2,
+           comment: 'It\'s your birthday, we\'re gonna party!',
+           author: '25 Cent',
+           date: '2011-12-02T17:57:28.556094Z'
+       }
+   ]
+};
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
+})
+export class DishdetailComponent implements OnInit {
+
+  dish = DISH;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
