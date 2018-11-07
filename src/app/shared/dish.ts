0ade57f2f49a8100868b86b1140d57d9fecd2989
Prabesh Bhaskaran <prabeshb@gmail.com>
2018-11-07 13:20:50 -0500

    Data Binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish {
   id: string;
   name: string;
@@ -7,4 +9,5 @@ export class Dish {
   label: string;
   price: string;
   description: string;
+  comments: Comment[];
 }
