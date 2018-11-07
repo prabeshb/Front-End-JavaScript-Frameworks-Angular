a8f164ff06a966e5a00ac47656d025c717fc0949
Prabesh Bhaskaran <prabeshb@gmail.com>
2018-11-07 15:29:28 -0500

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+
 
 @NgModule({
   declarations: [
@@ -31,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES} from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,16 +10,18 @@ import { DISHES} from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes =  DISHES;
+  dishes: Dish[];
 
   selectedDish: Dish;
 
-
-
-  constructor() { }
+  constructor(private dishService: DishService ) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
+
+
+
 
   onSelect(dish: Dish) {
     this.selectedDish = dish;
