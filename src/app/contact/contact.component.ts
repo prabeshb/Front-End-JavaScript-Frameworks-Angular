7875de2cb805f3d5409e4736cd42b02a73f9cb2a
Prabesh Bhaskaran <prabeshb@gmail.com>
2018-11-20 17:04:21 -0500

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,6 +14,34 @@ export class ContactComponent implements OnInit {
   contactType = ContactType;
   @ViewChild('fform') feedbackFormDirective;
 
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required': 'First name is required.',
+      'minlength': 'First name must be at least 2 characters long',
+      'maxlength': 'First name cannot be more than 25 charcters long'
+    },
+    'lastname': {
+      'required': 'Last name is required.',
+      'minlength': 'Last name must be at least 2 characters long',
+      'maxlength': 'Last name cannot be more than 25 charcters long'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format'
+    }
+  };
+
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
@@ -23,16 +51,40 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
   }
 
+  onValueChanged(data?: any) {
+    if(!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors ) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -25,7 +25,7 @@ export class DishdetailComponent implements OnInit {
     this.dishService.getDishIds()
       .subscribe((dishIds) => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id) });
+        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
 
   setPrevNext(dishIds: string) {
