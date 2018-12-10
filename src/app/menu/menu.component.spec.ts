f156a0a1bd1a4cae6b46cc84a4f1751e983fcc18
Prabesh Bhaskaran <prabeshb@gmail.com>
2018-12-10 09:21:21 -0500

    Testing part 1

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/process-httpmsg.service';
+import { DISHES } from '../shared/dishes';
+import { baseURL } from '../shared/baseurl';
+import { Observable, of } from 'rxjs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MenuComponent } from './menu.component';
+import { Browser } from 'protractor';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+
+    const DishServiceStub = {
+      getDishes: function(): Observable<Dish []> {
+        return of(DISHES);
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [
+        BrowserAnimationModule,
+        FlexLayoutModule,
+        RouterTestingModule.withRoutes([{ path: 'menu', component: MenuComponent}]),
+        MatGridListModule,
+        MatProgressSpinnerModule
+      ],
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: DishService, useValue: DishServiceStub },
+        { provide: 'BaseURL', useValue: baseURL }
+      ]
     })
     .compileComponents();
+
+    const dishservice = TestBed.get(DishService);
+
   }));
 
   beforeEach(() => {
